ffe592e9906dbf89b2ff3a6287600c39183a67b4
Deepak Suresh <deepusuresh1212@gmail.com>
2023-11-04 22:49:22 +0530

    2nd Commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x8807766C5d7d4f20adabDaD7429E50Fdc236F83A";
 export const contractABI = abi.abi;
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.9',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/oBy3LMvdfhiSpcRJcC4hTy_tKPLu2xt2',
+      accounts: ['335163d8f008c6786758a747cd43d35ac3eaa112040a708ca2505ac01aeda2e1'],
+    },
+  },
 };
